8d56b75e746fcbda47c0ceb7e9266b66e0e0cd69
yangyuncai <yyc@yunlaba.com>
2017-06-09 16:53:16 +0800

    fix bug

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -54,7 +54,7 @@ $app->post('/login/auth', function () use ($app) {
                 $ar->nickname = "水果人" . uniqid();
                 $ar->save();
 
-                $id = Users::findFirst("'phone' = " . $data['phone'] . " and 'deleteflag' = 0")->id;
+                $id = Users::findFirst("phone = " . $data['phone'] . " and deleteflag = 0")->id;
             }
 
             $token = md5($id . $data['phone'] . $app->config->salt . $data['uuid']);
