2e3acb94563857ba4f73f78a0de256acd416c137
yangyuncai <yyc@yunlaba.com>
2017-06-14 11:33:27 +0800

    add product

--- app/controllers/product.php
+++ app/controllers/product.php
@@ -6,13 +6,5 @@ $app->get('/product/category', function () use ($app) {
     $phql = "SELECT id,text FROM product_category where deleteflag=0";
     $categorys = $app->db->fetchAll($phql);
 
-    $ret = [];
-    foreach($categorys as $item) {
-        $tmp = [];
-        $tmp['id'] = $item->id;
-        $tmp['text'] = $item->text;
-        $ret[] = $tmp;
-    }
-
-    return $ret;
+    return $categorys;
 });
