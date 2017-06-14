560fc36bba3b3af85a3640e98ff462767537b3a5
yangyuncai <yyc@yunlaba.com>
2017-06-14 10:47:26 +0800

    add product

---
+++ app/controllers/product.php
@@ -0,0 +1,18 @@
+<?php
+use Fruit\Model\Shops;
+
+// TODO店铺唯一性检测
+$app->get('/product/category', function () use ($app) {
+    $phql = "SELECT id,text FROM product_category where deleteflag=0";
+    $categorys = $app->db->executeQuery($phql);
+
+    $ret = [];
+    foreach($categorys as $item) {
+        $tmp = [];
+        $tmp['id'] = $item->id;
+        $tmp['text'] = $item->text;
+        $ret[] = $tmp;
+    }
+
+    return $ret;
+});
