0a9cca919613d49358446ad2848e2bb6a39b21e8
yangyuncai <yyc@yunlaba.com>
2017-06-22 11:19:16 +0800

    add userinfo

--- app/controllers/product.php
+++ app/controllers/product.php
@@ -65,3 +65,30 @@ $app->get('/product/list/{id:\d+}', function ($id) use ($app) {
 
     return $data;
 });
+
+$app->get('/product/list/recom', function () use ($app) {
+    $result = Products::find([
+        'conditions' => 'deleteflag = 0 and state = 2',
+        'order' => 'id desc',
+        'columns' => 'id,product_category_id,price_unit_id,name,price,pic_url,inventory'
+    ]);
+
+    $data = [];
+    foreach($result as $item) {
+        $tmp = [];
+        foreach($item as $k => $v) {
+            $tmp[$k] = $v;
+
+            if ($k == 'product_category_id') {
+                $tmp[$k] = $app->db->fetchOne("select text from product_category where id=" . $v)['text'];
+            }
+
+            if ($k == 'price_unit_id') {
+                $tmp[$k] = $app->db->fetchOne("select text from product_unit where id=" . $v)['text'];
+            }
+        }
+        $data[] = $tmp;
+    }
+
+    return $data;
+});
